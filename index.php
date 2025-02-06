4ef6c06f4cb8a13924978198dda539f5976adc68
Kaloyan Donev <donevkaloyan@gmail.com>
2025-02-06 14:28:55 +0200

    Fix formatting for example navbar.

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <link rel="stylesheet" type="text/css" href="styles/navbar.css">
   </head>
   <body>
-<div class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
-</div>
+    <div class="topnav">
+      <a class="active" href="#home">Home</a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+    </div>
   </body>
 </HTML>
